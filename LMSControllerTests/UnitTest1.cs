03e0a152487bf26132268f7960240d1f00a074ab
jacobdietz <jacobdietz2383@gmail.com>
2026-04-17 18:00:28 -0600

    pushing tests

--- LMSControllerTests/UnitTest1.cs
+++ LMSControllerTests/UnitTest1.cs
@@ -6,64 +6,181 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
+using Xunit;
 
 namespace LMSControllerTests
 {
     public class UnitTest1
     {
-        // Uncomment the methods below after scaffolding
-        // (they won't compile until then)
+        [Fact]
+        public void TestGetDepartments()
+        {
+            var db = MakeTinyDB();
+            var ctrl = new CommonController(db);
+            var result = ctrl.GetDepartments() as JsonResult;
+            dynamic x = result.Value;
 
-        //[Fact]
-        //public void Test1()
-        //{
-        //    // An example of a simple unit test on the CommonController
-        //    CommonController ctrl = new CommonController(MakeTinyDB());
+            Assert.Equal(1, x.Length);
+            Assert.Equal("CS", x[0].subject);
+        }
 
-        //    var allDepts = ctrl.GetDepartments() as JsonResult;
+        [Fact]
+        public void TestCreateDepartment()
+        {
+            var db = MakeTinyDB();
+            var ctrl = new AdministratorController(db);
+            var result = ctrl.CreateDepartment("MATH", "Mathematics") as JsonResult;
+            dynamic x = result.Value;
+            Assert.True((bool)x.success);
+            Assert.Equal(2, db.Departments.Count());
+        }
 
-        //    dynamic x = allDepts.Value;
+        [Fact]
+        public void TestCreateCourse()
+        {
+            var db = MakeTinyDB();
+            var ctrl = new AdministratorController(db);
+            var result = ctrl.CreateCourse("CS", 5530, "Database Systems") as JsonResult;
+            dynamic x = result.Value;
 
-        //    Assert.Equal( 1, x.Length );
-        //    Assert.Equal( "CS", x[0].subject );
-        //}
+            Assert.True((bool)x.success);
+            Assert.Equal(1, db.Courses.Count());
+        }
 
+        [Fact]
+        public void TestEnrollStudent()
+        {
+            var db = MakeClassDB();
+            var ctrl = new StudentController(db);
+            var result = ctrl.Enroll("CS", 5530, "Fall", 2025, "u0000001") as JsonResult;
+            dynamic x = result.Value;
 
-        ///// <summary>
-        ///// Make a very tiny in-memory database, containing just one department
-        ///// and nothing else.
-        ///// </summary>
-        ///// <returns></returns>
-        //LMSContext MakeTinyDB()
-        //{
-        //    var contextOptions = new DbContextOptionsBuilder<LMSContext>()
-        //    .UseInMemoryDatabase( "LMSControllerTest" )
-        //    .ConfigureWarnings( b => b.Ignore( InMemoryEventId.TransactionIgnoredWarning ) )
-        //    .UseApplicationServiceProvider( NewServiceProvider() )
-        //    .Options;
+            Assert.True((bool)x.success);
+            Assert.Equal(1, db.Enrolleds.Count());
+        }
 
-        //    var db = new LMSContext(contextOptions);
+        [Fact]
+        public void TestGetGPA()
+        {
+            var db = MakeTinyDB();
+            db.Enrolleds.Add(new Enrolled { UId = "u0000001", ClassId = 1, Grade = "A" });
+            db.Enrolleds.Add(new Enrolled { UId = "u0000001", ClassId = 2, Grade = "B+" });
+            db.SaveChanges();
 
-        //    db.Database.EnsureDeleted();
-        //    db.Database.EnsureCreated();
+            var ctrl = new StudentController(db);
+            var result = ctrl.GetGPA("u0000001") as JsonResult;
+            dynamic x = result.Value;
+            Assert.Equal(3.65, (double)x.gpa, 2);
+        }
 
-        //    db.Departments.Add( new Department { Name = "KSoC", Subject = "CS" } );
+        [Fact]
+        public void TestGradeSubmission()
+        {
+            var db = MakeGradeDB();
+            var ctrl = new ProfessorController(db);
+            var result = ctrl.GradeSubmission("CS", 5530, "Fall", 2025, "Homework", "HW1", "u0000001", 95) as JsonResult;
+            dynamic x = result.Value;
 
-        //    // TODO: add more objects to the test database
+            Assert.True((bool)x.success);
+            Assert.Equal((uint)95, db.Submissions.First().Score);
+            Assert.Equal("A", db.Enrolleds.First().Grade);
+        }
 
-        //    db.SaveChanges();
+        private LMSContext MakeTinyDB()
+        {
+            var options = new DbContextOptionsBuilder<LMSContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .UseApplicationServiceProvider(NewServiceProvider())
+            .Options;
+            var db = new LMSContext(options);
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            db.Departments.Add(new Department
+            {
+                Subject = "CS",
+                Name = "KSoC"
+            });
 
-        //    return db;
-        //}
+            db.SaveChanges();
+            return db;
+        }
+
+        private LMSContext MakeClassDB()
+        {
+            var db = MakeTinyDB();
+            db.Courses.Add(new Course
+            {
+                Subject = "CS",
+                Number = 5530,
+                Name = "Database Systems"
+            });
+
+            db.Classes.Add(new Class
+            {
+                ClassId = 1,
+                Subject = "CS",
+                Number = 5530,
+                Season = "Fall",
+                Year = 2025,
+                Location = "WEB 123",
+                StartTime = new TimeOnly(9, 0, 0),
+                EndTime = new TimeOnly(10, 15, 0),
+                Professor = "p0000001"
+            });
+
+            db.SaveChanges();
+            return db;
+        }
+
+        private LMSContext MakeGradeDB()
+        {
+            var db = MakeClassDB();
+            db.AssignmentCategories.Add(new AssignmentCategory
+            {
+                Name = "Homework",
+                ClassId = 1,
+                Weight = 100
+            });
+
+            db.Assignments.Add(new Assignment
+            {
+                AssignmentId = 1,
+                ClassId = 1,
+                Name = "HW1",
+                Points = 100,
+                Due = DateTime.Now,
+                Content = "Test assignment",
+                CategoryName = "Homework"
+            });
+
+            db.Enrolleds.Add(new Enrolled
+            {
+                UId = "u0000001",
+                ClassId = 1,
+                Grade = "--"
+            });
+
+            db.Submissions.Add(new Submission
+            {
+                SubmissionId = 1,
+                UId = "u0000001",
+                AId = 1,
+                Time = DateTime.Now,
+                Score = 0,
+                Contents = "My submission"
+            });
+
+            db.SaveChanges();
+            return db;
+        }
 
         private static ServiceProvider NewServiceProvider()
         {
-            var serviceProvider = new ServiceCollection()
-          .AddEntityFrameworkInMemoryDatabase()
-          .BuildServiceProvider();
-
-            return serviceProvider;
+            return new ServiceCollection()
+            .AddEntityFrameworkInMemoryDatabase()
+            .BuildServiceProvider();
         }
-
     }
 }
