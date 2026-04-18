c0478bf294c2f3c82b3e9539d5a3b44e1e155149
jacobdietz <jacobdietz2383@gmail.com>
2026-04-17 21:23:04 -0600

    finished

--- LMS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LMS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -22,7 +21,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
-
 namespace LMS.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
@@ -34,7 +32,6 @@ namespace LMS.Areas.Identity.Pages.Account
         private readonly ILogger<RegisterModel> _logger;
         private readonly LMSContext db;
         //private readonly IEmailSender _emailSender;
-
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             IUserStore<ApplicationUser> userStore,
@@ -51,121 +48,94 @@ namespace LMS.Areas.Identity.Pages.Account
             db = _db;
             //_emailSender = emailSender;
         }
-
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         [BindProperty]
         public InputModel Input { get; set; } = new InputModel();
-
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public string ReturnUrl { get; set; }
-
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public class InputModel
         {
-
             [Required]
-            [Display( Name = "Role" )]
+            [Display(Name = "Role")]
             public string Role { get; set; }
-
             public List<SelectListItem> Roles { get; } = new List<SelectListItem>
             {
                 new SelectListItem { Value = "Student", Text = "Student" },
                 new SelectListItem { Value = "Professor", Text = "Professor" },
                 new SelectListItem { Value = "Administrator", Text = "Administrator"  }
             };
-
             public string Department { get; set; }
-
             public List<SelectListItem> Departments { get; set; } = new List<SelectListItem>
             {
                 new SelectListItem{Value = "None", Text = "NONE"}
             };
-
             [Required]
-            [Display( Name = "First Name" )]
+            [Display(Name = "First Name")]
             public string FirstName { get; set; }
-
             [Required]
-            [Display( Name = "Last Name" )]
+            [Display(Name = "Last Name")]
             public string LastName { get; set; }
-
             [Required]
-            [Display( Name = "Date of Birth" )]
-            [BindProperty, DisplayFormat( DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true )]
-            [DataType( DataType.Date )]
+            [Display(Name = "Date of Birth")]
+            [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            [DataType(DataType.Date)]
             public System.DateTime DOB { get; set; } = DateTime.Now;
-
             [Required]
             //[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType( DataType.Password )]
-            [Display( Name = "Password" )]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
             public string Password { get; set; }
-
-            [DataType( DataType.Password )]
-            [Display( Name = "Confirm password" )]
-            [Compare( "Password", ErrorMessage = "The password and confirmation password do not match." )]
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-
         }
-
-
-        public async Task OnGetAsync( string returnUrl = null )
+        public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
-            ExternalLogins = ( await _signInManager.GetExternalAuthenticationSchemesAsync() ).ToList();
-
-
-
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
-
-        public async Task<IActionResult> OnPostAsync( string returnUrl = null )
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content( "~/" );
-            ExternalLogins = ( await _signInManager.GetExternalAuthenticationSchemesAsync() ).ToList();
-            if ( ModelState.IsValid )
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
             {
                 var uid = CreateNewUser(Input.FirstName, Input.LastName, Input.DOB, Input.Department, Input.Role);
                 var user = new ApplicationUser { UserName = uid };
-
-                await _userStore.SetUserNameAsync( user, uid, CancellationToken.None );
+                await _userStore.SetUserNameAsync(user, uid, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
-
-                if ( result.Succeeded )
+                if (result.Succeeded)
                 {
-                    _logger.LogInformation( "User created a new account with password." );
-                    await _userManager.AddToRoleAsync( user, Input.Role );
-
+                    _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, Input.Role);
                     var userId = await _userManager.GetUserIdAsync(user);
-
-                    await _signInManager.SignInAsync( user, isPersistent: false );
-                    return LocalRedirect( returnUrl );
-
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
-                foreach ( var error in result.Errors )
+                foreach (var error in result.Errors)
                 {
-                    ModelState.AddModelError( string.Empty, error.Description );
+                    ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-
             // If we got this far, something failed, redisplay form
             return Page();
         }
-
         private IdentityUser CreateUser()
         {
             try
@@ -174,14 +144,12 @@ namespace LMS.Areas.Identity.Pages.Account
             }
             catch
             {
-                throw new InvalidOperationException( $"Can't create an instance of '{nameof( ApplicationUser )}'. " +
-                    $"Ensure that '{nameof( IdentityUser )}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml" );
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
+                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
         }
-
         /*******Begin code to modify********/
-
         /// <summary>
         /// Create a new user of the LMS with the specified information and add it to the database.
         /// Assigns the user a unique uID consisting of a 'u' followed by 7 digits.
@@ -192,11 +160,44 @@ namespace LMS.Areas.Identity.Pages.Account
         /// <param name="departmentAbbrev">The department abbreviation that the user belongs to (ignore for Admins) </param>
         /// <param name="role">The user's role: one of "Administrator", "Professor", "Student"</param>
         /// <returns>The uID of the new user</returns>
-        string CreateNewUser( string firstName, string lastName, DateTime DOB, string departmentAbbrev, string role )
+        string CreateNewUser(string firstName, string lastName, DateTime dob, string departmentAbbrev, string role)
         {
-            return "unknown";
+            string uid = "u" + new Random().Next(1000000, 9999999).ToString();
+            if (role == "Student")
+            {
+                db.Students.Add(new Student
+                {
+                    UId = uid,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Dob = DateOnly.FromDateTime(dob),
+                    Major = departmentAbbrev
+                });
+            }
+            else if (role == "Professor")
+            {
+                db.Professors.Add(new Professor
+                {
+                    UId = uid,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Dob = DateOnly.FromDateTime(dob),
+                    Dept = departmentAbbrev
+                });
+            }
+            else if (role == "Administrator")
+            {
+                db.Administrators.Add(new Administrator
+                {
+                    UId = uid,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Dob = DateOnly.FromDateTime(dob)
+                });
+            }
+            db.SaveChanges();
+            return uid;
         }
-
         /*******End code to modify********/
     }
 }
